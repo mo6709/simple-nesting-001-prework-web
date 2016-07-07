1c2677b6f7d2f7fd1251fb8552aa5d7048501b65
mo6709 <mo6709@gmail.com>
2016-07-07 13:43:42 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,5 @@
 
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -15,7 +16,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+   programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -77,9 +81,12 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
+        },
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LIPS","c"]
         }
      }
-
     
 end
 
@@ -101,6 +108,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
      
 end
@@ -121,10 +130,10 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
-     
+     programmer_hash[:dennis_ritchie][:languages][1] = "Assembly" 
+     programmer_hash
 end
-
+# binding.pry
 
 
 
